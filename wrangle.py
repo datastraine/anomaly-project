72499b589fd86538eabf1b3520ca725d3da5b005
datastraine <anthony.straine@gmail.com>
2020-11-11 21:03:55 -0600

    updates wrangle moduel and the exploration notebook

--- wrangle.py
+++ wrangle.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import numpy as np
 
-'''
-Files for this ask were provided off network and connect be retrived from the web. You must request access to them from the DBA
 
-* Pulls in the CodeUp anonymized student access data from a text file into a DF and renames the columns .
-* Merges the cohort data from the cohort.csv file with the student data
-* Sets the index to datetime
-* Creates a classtype, program, and location columns from program_id...
-   and knowing which cohort belongs to which program ID
-* Creates seperate data frames for webdev and data science (ds) users
-* returns data, webdev, and ds dataframes
-'''
 def student_data():
+    '''
+    Files for this ask were provided off network and connect be retrived from the web. You must request access to them from the DBA
+
+    * Pulls in the CodeUp anonymized student access data from a text file into a DF and renames the columns .
+    * Merges the cohort data from the cohort.csv file with the student data
+    * Sets the index to datetime
+    * Creates a classtype, program, and location columns from program_id...
+    and knowing which cohort belongs to which program ID
+    * Creates seperate data frames for webdev and data science (ds) users
+    * returns data, webdev, ds, and cohort dataframes (must grab all in this order)
+    '''
+
     # Reads the data from the anonymized-curriculum-access.txt into a DF
     data = pd.read_csv('anonymized-curriculum-access.txt', sep=" ", header=None, na_values='"-"')
     # Rename columns as needed
@@ -37,7 +39,7 @@ def student_data():
     data.loc[data['program_id'] == 3, 'classtype'] = 'DataScience-SanAntonio'
     data.loc[data['program_id'] == 4, 'classtype'] = 'Web-Houston'
     #Then split the class type column into two on -
-    data[['program','location']] = data.classtype.str.split("-",expand=True) 
+    data[['program','location']] = data.classtype.str.split("-",expand=True)
     # Create seperate data fames for web and datascience
     web = data[data['program']=='Web']
     ds = data[data['program']=='DataScience']
@@ -47,4 +49,5 @@ def student_data():
     # set the index to datetime
     ds=ds.set_index('datetime')
     web=web.set_index('datetime')
-    return data, web, ds
+    data=data.set_index('datetime')
+    return data, web, ds, cohort
